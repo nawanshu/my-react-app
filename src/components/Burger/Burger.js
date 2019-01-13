18241c7446b1413fbd7471412da6e2425af1a7bc
Nawanshu Choudhary <nawanshu@gmail.com>
2019-01-13 16:52:08 +0530

    Complete burger control module

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,7 +4,7 @@ import classes from './Burger.css';
 import BurgerIngredient  from './BurgerIngredient/Burgeringredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => {
                 return <BurgerIngredient key={igKey + 1} type={igKey} />;
@@ -12,6 +12,10 @@ const burger = (props) => {
         }).reduce((arr, el) => {
             return arr.concat(el);
         }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
 
     return (
         <div className={classes.Burger}>
